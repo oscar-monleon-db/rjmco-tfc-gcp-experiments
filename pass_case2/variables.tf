b5b1b87cbae6bb8b76ddd935686c8a0d2bfaf29f
Ricardo Cordeiro <Ricardo.Cordeiro@gft.com>
2021-02-21 14:33:06 +0000

    Adds pass_case2

---
+++ pass_case2/variables.tf
@@ -0,0 +1,10 @@
+### The following variable will be set by the user.
+variable "host_project_id" {
+  description = "GCP host project ID"
+  type        = string
+}
+
+variable "service_project_id" {
+  description = "GCP service project ID"
+  type        = string
+}
