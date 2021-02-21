1f1bf0ac7534c0ecca75d6e84153df9191cdc659
Ricardo Cordeiro <Ricardo.Cordeiro@gft.com>
2021-02-21 15:01:48 +0000

    Fixes pass_case3 interpolation warnings

--- pass_case3/main.tf
+++ pass_case3/main.tf
@@ -2,7 +2,7 @@ module "pass_case3" {
   source  = "app.terraform.io/rjmco/sentinel-illegal-module/google"
   version = "0.0.1+experiment.0"
 
-  environment = "${var.environment}"
-  organization = "${var.organization}"
-  service_project_id = "${var.service_project_id}"
+  environment = var.environment
+  organization = var.organization
+  service_project_id = var.service_project_id
 }
