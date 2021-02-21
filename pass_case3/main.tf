dcdcd947ed1260930ad725c0718fd61ffce57866
Ricardo Cordeiro <Ricardo.Cordeiro@gft.com>
2021-02-21 16:58:32 +0000

    Bumping to latest module versions

--- pass_case3/main.tf
+++ pass_case3/main.tf
@@ -1,8 +1,8 @@
 module "pass_case3" {
   source  = "app.terraform.io/rjmco/sentinel-illegal-module/google"
-  version = "0.0.1+experiment.1"
+  version = "0.0.1+experiment.2"
 
-  environment = var.environment
-  organization = var.organization
+  environment        = var.environment
+  organization       = var.organization
   service_project_id = var.service_project_id
 }
