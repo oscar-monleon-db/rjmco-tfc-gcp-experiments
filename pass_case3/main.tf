08ba4bcbe89fc20cf5630a785d637472ccc44b09
Ricardo Cordeiro <Ricardo.Cordeiro@gft.com>
2021-02-21 14:58:57 +0000

    Fix pass_case3 variable names

--- pass_case3/main.tf
+++ pass_case3/main.tf
@@ -1,16 +1,8 @@
-//--------------------------------------------------------------------
-// Variables
-variable "sentinel_illegal_module_environment" {}
-variable "sentinel_illegal_module_organization" {}
-variable "sentinel_illegal_module_service_project_id" {}
-
-//--------------------------------------------------------------------
-// Modules
-module "sentinel_illegal_module" {
+module "pass_case3" {
   source  = "app.terraform.io/rjmco/sentinel-illegal-module/google"
   version = "0.0.1+experiment.0"
 
-  environment = "${var.sentinel_illegal_module_environment}"
-  organization = "${var.sentinel_illegal_module_organization}"
-  service_project_id = "${var.sentinel_illegal_module_service_project_id}"
+  environment = "${var.environment}"
+  organization = "${var.organization}"
+  service_project_id = "${var.service_project_id}"
 }
